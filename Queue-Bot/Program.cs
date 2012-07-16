e5c6fd9230c9908a47aae9b3bd67ab020ba760bd
Raymond Price <raymond@4-tell.com>
2012-07-16 13:54:52 -0800

    Initial commit. Just putting all the files together.

---
+++ Queue-Bot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Plan:
+ * 1: PQueue to hold the jobs. 
+ * 1a: Jobs class; Time needed, cost of time, name of job.
+ * 1b: Sorting- find min(sum(Ti * Pi)). 
+ * 2: Find BEWT; sum(Pi * Ti - BEWT) = balance
+ * 3: Work out how much everybody owes.
+ */
+namespace Queue_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class PQueue
+    {
+    }
+}
