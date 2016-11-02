3e89aad6a4f0aa8e5148f4ab127ea18f47b8ba2a
Raymond <Raymond@KATERINA>
2016-11-01 21:07:32 -0700

    Fixed routing to be Angular-router-friendly. Next time I rework all this I'm going to do it right, but for now I just want to get something working, then I'll pretty it up.

--- FrontendMk2/App_Start/RouteConfig.cs
+++ FrontendMk2/App_Start/RouteConfig.cs
@@ -13,11 +13,25 @@ namespace FrontendMk2
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //We're going to do all the routing through the Angular router. 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+               name: "default",
+       url: "Home/Index"
+       );
+
+            // when the user types in a link handled by client side routing to the address bar 
+            // or refreshes the page, that triggers the server routing. The server should pass 
+            // that onto the client, so Angular can handle the route
+            routes.MapRoute(
+                name: "spa-fallback",
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "Index" }
             );
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
