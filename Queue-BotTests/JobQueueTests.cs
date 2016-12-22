c43456c8c4363d998ecdc00382940b41fc48174d
Raymond <darthbob88@gmail.com>
2016-12-22 13:55:43 -0800

    First steps towards adding proper testing for QBot.

---
+++ Queue-BotTests/JobQueueTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Queue_Bot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue_Bot.Tests
+{
+    public class JobQueueTests
+    {
+        JobQueue testingQueue;
+        public JobQueueTests()
+        {
+            testingQueue = JobQueue
+        }
+        [Fact()]
+        public void AddCustomerTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+    }
+}
