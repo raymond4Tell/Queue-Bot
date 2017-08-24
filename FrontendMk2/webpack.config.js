99195584762ab32a3706394a2a650780d316a7cd
v-raypri <v-raypri@V-RAYPRI021017>
2017-08-24 14:11:41 -0700

    Removing the compiled JS files, since apparently they got added to the project as well.

--- FrontendMk2/webpack.config.js
+++ FrontendMk2/webpack.config.js
@@ -1,22 +1,22 @@
 ﻿/// <binding ProjectOpened='Watch - Development' />
 module.exports = {
-  entry: "./Scripts/app/index.tsx",
-  output: {
-    filename: "./Scripts/bundle.js",
-  },
+	entry: "./Scripts/app/index.tsx",
+	output: {
+		filename: "./Scripts/bundle.js",
+	},
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+	// Enable sourcemaps for debugging webpack's output.
+	devtool: "source-map",
 
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-  },
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+	},
 
-  module: {
-    loaders: [
-              // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-                      { test: /\.tsx?$/, loader: "ts-loader" }
-    ]
-  }
+	module: {
+		loaders: [
+				  // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+						  { test: /\.tsx?$/, loader: "ts-loader" }
+		]
+	}
 }
