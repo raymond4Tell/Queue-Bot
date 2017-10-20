163e9037941fda51ef9676d450f7a930f79f492d
Raymond Price <v-raypri@microsoft.com>
2017-10-20 14:38:38 -0700

    Pulled out single task component.

--- FrontendMk2/Scripts/app/components/Dashboard.tsx
+++ FrontendMk2/Scripts/app/components/Dashboard.tsx
@@ -1,26 +1,29 @@
 ﻿import * as React from "react";
-import { QueueDTO } from "../types/Model";
+import * as QBot from "../types/Model";
 import { StoreState } from "./../Index";
 import Link from "redux-first-router-link";
 
 import Moment from "react-moment";
 // TODO: Find better way to display the BEWT with some measure of precision.
-export const Dashboard = (currentQueue: QueueDTO) => {
+export const Dashboard = (currentQueue: QBot.QueueDTO) => {
 	return <div>
 		<div className="col-md-3" >BEWT:  {currentQueue.bewt.humanize()} </div>
 		<div className="col-md-3" >Current Balance: {currentQueue.machineBalance}</div>
 		<ul>
-			{currentQueue.internalQueue.map(task =>
-				<li className='list-group-item' key={task.taskId} >
-					taskID: {task.taskId}<br />
-					Customer: {task.customer.name}<br />
-					Job: {task.job.name}<br />
-					TimePrice: {task.timePrice} <br />
-					Went on Queue: {task.timeEnqueued}<br />
-					Balance: {task.Balance}<br />
-					jobId: {task.job.jobId}
-				</li>)
+            {currentQueue.internalQueue.map(task =>
+                <SingleTask key={task.taskId} {...task} />
+			)
 			}
 		</ul>
 	</div >;
 }
+export const SingleTask = (task: QBot.Task) => 
+    <li className='list-group-item' key={task.taskId} >
+        taskID: {task.taskId}<br />
+        Customer: {task.customer.name}<br />
+        Job: {task.job.name}<br />
+        TimePrice: {task.timePrice} <br />
+        Went on Queue: {task.timeEnqueued}<br />
+        Balance: {task.Balance}<br />
+        jobId: {task.job.jobId}
+    </li>
