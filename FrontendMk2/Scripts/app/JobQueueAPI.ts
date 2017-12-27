272487c4b7a694361e651ee03367736e8ad356ed
Raymond Price <v-raypri@microsoft.com>
2017-12-27 14:51:59 -0800

    Rounded out the JobQueueAPI, for later use.

--- FrontendMk2/Scripts/app/JobQueueAPI.ts
+++ FrontendMk2/Scripts/app/JobQueueAPI.ts
@@ -1,23 +1,64 @@
 ﻿import { Task } from "./types/Model";
 
 class JobQueueApi {
+    private static taskUrl = 'api/Queue/Tasks';
+    private static jobUrl = 'api/Queue/Jobs';
+    private static customerUrl = 'api/Queue/Customers';
+    private static dashUrl = 'api/Queue/Queue';
+    private static headers = new Headers({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionStorage.getItem("auth_token")
+    });
+
     static getAllJobs() {
-        return fetch('api/Queue/Jobs').then(response => {
-            return response.json();
-        }).catch(error => {
-            return error;
-        });
+        return fetch(this.jobUrl).then(response => response.json()
+        ).catch(error => error);
     }
+
     static createTask(task: Task) {
-      return  fetch('api/Queue/Tasks', {
-          method: "POST",
-          body: JSON.stringify(task),
-          headers: {"Content-Type": "application/json"}
-        }).then(response => {
-                return response.json();
-            }).catch(error => {
-                return error;
-            });
+        return fetch(this.taskUrl, {
+            method: "POST",
+            body: JSON.stringify(task),
+            headers: this.headers
+        })
+            .then(response => response.json())
+            .catch(error => error);
+    }
+    
+    static getallTasks() {
+        return fetch(this.taskUrl)
+            .then(response => response.json())
+            .catch(error => error);
+    }
+
+    static getCustomers() {
+        return fetch(this.customerUrl)
+            .then(response => response.json())
+            .catch(error => error);
+    }
+
+    static getTask(id: string) {
+        const url = `${this.taskUrl}/${id}`;
+        return fetch(url)
+            .then(response => response.json())
+            .catch(error => error);
+    }
+
+    static getQueueStatus() {
+        return fetch(this.dashUrl)
+            .then(response => response.json())
+            .catch(error => error);
+    }
+
+    static update(task: Task) {
+        const url = `${this.taskUrl}/${task.taskId}`;
+        return fetch(url, {
+            method: "put",
+            body: JSON.stringify(task),
+            headers: this.headers
+        })
+            .then(response => response.json())
+            .catch(error => error);
     }
 }
 
