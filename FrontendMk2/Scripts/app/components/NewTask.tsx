e90ddd0749044eaf94363e26907bb9df392ce0a7
Raymond Price <v-raypri@microsoft.com>
2018-05-13 15:43:50 -0700

    Fixed #2.
    Now we can actually register a new customer as part of registering a new task.

--- FrontendMk2/Scripts/app/components/NewTask.tsx
+++ FrontendMk2/Scripts/app/components/NewTask.tsx
@@ -11,12 +11,12 @@ export interface NewTaskFormProps {
     jobList: QBot.Job[],
     customerList: QBot.Customer[]
 }
-
 type NewTaskState = {
     newTask: QBot.Task
 };
 
 class NewTaskForm extends React.Component<NewTaskFormProps, NewTaskState> {
+    newCustomer: HTMLInputElement;
     state: NewTaskState = {
         newTask: QBot.emptyTask
     };
@@ -30,7 +30,18 @@ class NewTaskForm extends React.Component<NewTaskFormProps, NewTaskState> {
 
     // TODO: Add better validation, here and server-side.
     sendTask = () => {
+        if (this.newCustomer) {
+            this.state.newTask.authId = this.state.newTask.customer.authId;
+        }
         this.props.createTask(this.state.newTask);
+    }
+    handleCustomer = (event) => {
+        const { customer } = this.state.newTask;
+        const name = event.target.name;
+        const value = event.target.value;
+        this.setState({
+            newTask: { ...this.state.newTask, customer: { ...this.state.newTask.customer, [name]: value } }
+        });
     }
 
     render() {
@@ -49,14 +60,24 @@ class NewTaskForm extends React.Component<NewTaskFormProps, NewTaskState> {
                 )
                 }
             </fieldset>
-            // TODO: Make this into an "insert current details here, or insert new details" sort of thing.
-            <label>Customer AuthID
+            {/* TODO: Make this into an "insert current details here, or insert new details" sort of thing. */}
+            <fieldset>
+                <input type="checkbox" ref={(input) => this.newCustomer = input} />
+                <label>Customer AuthID
                 <select value={this.state.newTask.authId} name="authId" onChange={this.handleIncrement}>
-                    {customerList.map(customer =>
-                        <option key={customer.authId} value={customer.authId} > { customer.name } </option>)}
-                </select>
+                        {customerList.map(customer =>
+                            <option key={customer.authId} value={customer.authId} > {customer.name} </option>)}
+                    </select>
                 </label>
-           
+                Alternative ID
+
+                <label>Customer ID
+                <input value={this.state.newTask.customer.authId} name="authId" onChange={this.handleCustomer} type="text" />
+                </label>
+                <label>Customer Name
+                <input value={this.state.newTask.customer.name} name="name" onChange={this.handleCustomer} type="text" />
+                </label>
+            </fieldset>
             <label>Time Price
                 <input type="number" name="timePrice" onChange={this.handleIncrement} value={this.state.newTask.timePrice} />
             </label>
